584853fb03ba91d11d3d5b09e2cdb513737d1ee0
Andrzej Nagórko <amn@mimuw.edu.pl>
2016-02-14 14:29:14 +0100

    3x1 w nowym katalogu

--- home/anagorko/lekcje/debugger/3x1.cpp
+++ home/anagorko/lekcje/debugger/3x1.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
+using namespace std;
 
 int main(int argc, char **argv)
 {
-    int x = 27;
+    int x = 27,i=0;
     
     while (x != 1) {
         if (x % 2 == 0) {
             x = x / 2;
+            i++;
         } else {
             x = 3 * x + 1;
+            i++;
         }
     }
+    cout<<i;
     
     return 0;
 }
