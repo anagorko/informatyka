2be242aaaffb52c6c77a159847eef729c563ab81
Jajanek98 <Janek.lukomski98@gmail.com>
2016-05-20 11:28:11 +0200

    szkica adressbook.cpp

--- home/lukom/adress_book.cpp
+++ home/lukom/adress_book.cpp
@@ -1,33 +1,55 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <fstream>
+
 using namespace std;
 
 class Person{
+
+    int id;
+
+    vector<char> name;
+    vector<char> surname;
+    vector<char> mail;
+    vector<int> phone;
 
 };
 class adressbook {
 
 	vector<Person> bk;
 
-	void write(string bookname) {
-        
-        fstream bookfile;
+    void read(string bookname);
+	void write(string bookname);
 
-        bookfile.open(bookname, ios::trunc);
-
-        for (Person& p: bk) {
-            bookfile << p;
-        }
-
-        bookfile.close();
-    }
 
 };
-class UI {
-
+class UI {//User Interface
+public:
 	void help();
+
+    void list();//TODO
+    void add();//TODO
+    void modify();//TODO
+    void del();//TODO
+
 };
+
+void UI::help(){
+    cout<<"Witaj w ksiazce adresowej!\n";
+}
+
+void adressbook::write(string bookname){
+    
+    fstream bookfile;
+
+    bookfile.open(bookname, ios::trunc);
+
+    int p = 1;
+    bookfile << p;
+
+    bookfile.close();
+}
 
     
 
@@ -35,8 +57,8 @@ int main(int argc, char* argv[])
 {
     UI iface;
 
-//    if (argc == 1 || (argc > 1 && string(argv[1]) == "help")) {
-//        iface.help(); 
-//        return 0;
-//    }
+    if (argc == 1 || (argc > 1 && string(argv[1]) == "help")) {
+        iface.help(); 
+        return 0;
+    }
 }
