5eb2f2520d0ad2bf0be43e0f1a551f1d05e8e1d2
Miziol <k.miziolek08@gmail.com>
2016-05-23 18:02:47 +0200

    Kolejne poprawki

--- home/Miziol/Book_library.h
+++ home/Miziol/Book_library.h
@@ -21,8 +21,33 @@ class Book
 private:
 
 public:
-	void openFile(string file)
+	void openFile(char* file)
 	{
-		fstream
-	};
+		fstream bk;
+
+		int element;
+		string s;
+
+		bk.open(file, ios::app | ios::in | ios::out );
+		getline(bk, s);
+		for (int i = 1; i < s.size()-1; i++)
+		{
+			size = size * 10 + s[i];
+		}
+		for (int i = 0; i < size; i++)
+		{
+			getline(bk, s);
+
+			switch(element)
+			{
+				case 1:
+				if(s[i] == ';')
+				element++;
+				break;
+
+				case 2:
+			}
+
+		}
+	}
 };
