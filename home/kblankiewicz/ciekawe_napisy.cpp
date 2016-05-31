0060beb3cfc8ee96f6c354bdc6be243a6217c920
KrzysiekBlankiewicz <kblankiewicz5@gmail.com>
2016-05-31 09:22:16 +0200

    Działa

--- home/kblankiewicz/ciekawe_napisy.cpp
+++ home/kblankiewicz/ciekawe_napisy.cpp
@@ -7,18 +7,18 @@ using namespace std;
 
 bool pierwszosc(int z)
 {
-    int t[z];
+    int t[z+1];
     for(int i = 2; i <= z; i++)
     {
-        t[i] = 0;
+        t[i] = true;
     }
     t[0] = -1;
     t[1] = -1;
     for(int i = 2; i <= z; i++)
     {
-        if(t[i] == 0)
+        if(t[i] == true)
         {
-            for(int j = 2; j <= z/i + 1; j++)
+            for(int j = 2; j <= z/i; j++)
             {
                 t[i*j] = false;
             }
@@ -33,7 +33,10 @@ class Napis
     string n;
 
 public:
-    Napis();
+    Napis()
+	{
+
+	}
     Napis(string _n)
     {
         _n = n;
@@ -75,15 +78,14 @@ public:
     bool czyPierwszy() const
     {
         int x = 0;
-        string s = getNapis();
-        int q = s.length();
+        int q = n.length();
         for(int i = 0; i < q; i++)
         {
-            int y = (int) getNapis()[i];
-            cout<<"niby ascii"<< y << endl;
+            int y = (int) n[i];
+            //cout<<"niby ascii"<< y << endl;
             x = x + y;
         }
-        cout<<x<<endl;
+        //cout<<x<<endl;
         if(pierwszosc(x) == true)
         {
             return true;
@@ -92,13 +94,12 @@ public:
     }
     bool czyRosnacy() const
     {
-        string s = getNapis();
-        int q = s.length();
+        int q = n.length();
         int z = 0;
         for(int i = 0; i < q; i++)
         {
-            int a = (int) getNapis()[i];
-           // cout<<"a = "<<a<<endl;
+            int a = (int) n[i];
+            //cout<<"a = "<<a<<endl;
             if(a < z) return false;
             z = a;
         }
@@ -111,7 +112,7 @@ public:
 istream& operator>>(istream& is, Napis &n)
 {
     string x;
-    cin>>x;
+    is>>x;
     n.setNapis(x);
 }
 ostream& operator<<(ostream& os, const Napis &n)
@@ -121,27 +122,19 @@ ostream& operator<<(ostream& os, const Napis &n)
 
 int main()
 {
- /*   ifstream napisy;
-
+    ifstream napisy;
     napisy.exceptions(ios::failbit | ios::badbit);
     napisy.open("NAPIS.TXT");
-
     vector<Napis> t;
-
     for (int i = 0; i < 1000; i++) {
         Napis n;
         napisy >> n;
         t.push_back(n);
     }
-
     napisy.close();
-
     sort(t.begin(), t.end());
-
     bool wlasnie_byl = false;
-
     cout << "Duplikaty:";
-
     for (vector<Napis>::iterator i = t.begin(); i < t.end(); i++) {
         if (i + 1 < t.end() && *i == *(i+1)) {
             if (!wlasnie_byl) {
@@ -154,28 +147,16 @@ int main()
         }
     }
     cout << endl;
-
     int lp = 0;
     for (const Napis& n: t) {
         if (n.czyPierwszy()) lp++;
     }
-
     cout << endl << "Liczba napisów pierwszych: " << lp << endl << endl;
-
     cout << "Napisy rosn¹ce:";
-
     for (const Napis& n: t) {
         if (n.czyRosnacy()) { cout << " " << n; }
     }
     cout << endl << endl;
-
     return 0;
-    */
 
-    Napis m = Napis("x");
-    cin>>m;
-
-    if (m.czyPierwszy() == true) cout<< "pierwszy"<<endl;
-    if(m.czyRosnacy() == true) cout << "rosnacy"<<endl;
-
-    }
+}
