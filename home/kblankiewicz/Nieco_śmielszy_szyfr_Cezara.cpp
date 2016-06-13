2f963d73324240cb0b4f882e238072160d029c73
KrzysiekBlankiewicz <kblankiewicz5@gmail.com>
2016-06-13 22:24:29 +0200

    NIe do końca

--- home/kblankiewicz/Nieco_śmielszy_szyfr_Cezara.cpp
+++ home/kblankiewicz/Nieco_śmielszy_szyfr_Cezara.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include<fstream>
+#include<cmath>
 using namespace std;
+
+
+//int l = 0;
+//int q = 0;
 
 string zaszyfruj(string slowo, int klucz)
 {
@@ -13,13 +18,11 @@ string zaszyfruj(string slowo, int klucz)
     for(int i = 0; i < slowo.length(); i++)
     {
         ascii1 = (int) slowo[i];
-        cout<<"ascii1  "<<ascii1<<endl;
         ascii2 = ascii1 + klucz2;
         if(ascii2 > 90)
         {
-            ascii2 = 64 + (klucz - (90 - ascii1));
+            ascii2 = 65 + (klucz2 - (90 - ascii1));
         }
-        cout<<"ascii2  "<<ascii2<<endl;
         wynik[i] = (char) ascii2;
     }
     return wynik;
@@ -36,37 +39,14 @@ string rozszyfruj(string szyfr, int klucz)
     for(int i = 0; i < szyfr.length(); i++)
     {
         ascii1 = (int) szyfr[i];
-        cout<<"ascii1  "<<ascii1<<endl;
         ascii2 = ascii1 - klucz2;
         if(ascii2 < 65)
         {
             ascii2 = 91 - (klucz2 - (ascii1 - 65));
         }
-        cout<<"ascii2  "<<ascii2<<endl;
         wynik[i] = (char) ascii2;
     }
     return wynik;
-}
-
-void szyfrowanie(string dane, string wyniki)
-{
-    int klucz = 107;
-    string dozrobienia;
-
-    ifstream bookfile;
-    bookfile.open(dane.c_str(), ios::in);
-
-    ofstream plik;
-    ofstream.open(wyniki.c_str(), ios::trunc);
-
-    for(int i = 0; i < 100; i++)
-    {
-        bookfile >> dozrobienia;
-        plik << zaszyfruj(dozrobienia, klucz);
-    }
-
-    bookfile.close();
-    plik.close;
 }
 
 int blad(string slowo, string szyfr)
@@ -81,35 +61,96 @@ int blad(string slowo, string szyfr)
 		ascii1 = (int) slowo[i];
 		ascii2 = (int) szyfr[i];
 
-		klucz1 = ascii1 - ascii2;
+		klucz1 = abs(ascii1 - ascii2);
 		if(i > 0)
 		{
 			if(klucz1 != klucz2)
 			{
+			    //l++;
 				return 1;
 			}
 		}
 		klucz2 = klucz1;
 	}
+	//q++;
+    return 0;
+}
+
+void szyfrowanie(string dane, string wyniki)
+{
+    int klucz = 107;
+    string dozrobienia;
+
+    ifstream wejscie;
+    wejscie.open(dane.c_str(), ios::in);
+
+    ofstream wyjscie;
+    wyjscie.open(wyniki.c_str(), ios::trunc);
+
+    for(int i = 0; i < 100; i++)
+    {
+        wejscie >> dozrobienia;
+        wyjscie << zaszyfruj(dozrobienia, klucz) << endl;
+    }
+
+    wejscie.close();
+    wyjscie.close();
+}
+
+void rozszyfrowywanie(string dane, string wyniki)
+{
+    int aktualny_klucz;
+    string do_zrobienia;
+
+    ifstream wejscie;
+    wejscie.open(dane.c_str(), ios::in);
+
+    ofstream wyjscie;
+    wyjscie.open(wyniki.c_str(), ios::trunc);
+
+    for(int i = 0; i < 3000; i++)
+    {
+        wejscie >> do_zrobienia >> aktualny_klucz;
+        wyjscie << rozszyfruj(do_zrobienia, aktualny_klucz) << endl;
+    }
+
+    wejscie.close();
+    wyjscie.close();
+}
+
+void czy_blad(string dane, string wynik)
+{
+    string aktualne_slowo;
+    string aktualny_szyfr;
+
+    ifstream wejscie;
+    wejscie.open(dane.c_str(), ios::in);
+
+    ofstream wyjscie;
+    wyjscie.open(wynik.c_str(), ios::trunc);
+
+    for(int i = 0; i < 3000; i++)
+    {
+        wejscie >> aktualne_slowo >> aktualny_szyfr;
+        if(blad(aktualne_slowo, aktualny_szyfr) == 1)
+        {
+            cout<<aktualne_slowo<<" "<<aktualny_szyfr<<endl;
+            wyjscie << aktualne_slowo << endl;
+        }
+    }
+
+    wejscie.close();
+    wyjscie.close();
 }
 
 int main()
 {
-   szyfrowanie("dane_6_1.txt", "wyniki_6_1.txt");
+    
+    szyfrowanie("dane_6_1.txt", "wyniki_6_1.txt");
 
-    /*for(int i = 0; i < 3000; i++)
-    {
-        string x;
-        int y;
-        cin>>x>>y;
-        cout<<rozszyfruj(x, y);
-    }
+    rozszyfrowywanie("dane_6_2.txt", "wynik_6_2.txt");
 
-    for(int i = 0; i < 3000; i++)
-    {
-        string a, b;
-        cin>>a>>b;
-        cout<<blad(a, b); //zwrócenie 1 oznacza błąd
-    }
-*/
+    czy_blad("dane_6_3.txt", "wynik_6_3.txt");
+    //cout<<l<<endl<<q;
+    
 }
