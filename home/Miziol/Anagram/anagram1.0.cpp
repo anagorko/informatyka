9ea493eef3d99604349b4ab9ef8f32022ec15729
Miziol <k.miziolek08@gmail.com>
2016-09-05 10:40:28 +0200

    Coś tam z lekcji

---
+++ home/Miziol/Anagram/anagram1.0.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+string anagram(string s)
+{
+	string w = "";
+	for (int i = 97; i <= 122; i++)
+	{
+		for (int j = 0; j < s.size(); j++)
+		{
+		(int) s[j] == i;
+		w =+ (char)i;
+		}
+	}
+
+	return w;
+}	
+
+int main()
+{
+	string in = " ";
+	bool out;
+	vector <string> s;
+
+	while (in != "aaa")
+	{
+		cin >> in;
+		s.push_back(in);
+
+	}
+
+	for (int i = 0; i < s.size(); i++)
+	{
+		out = 0;
+		for (int j = i + 1; j <= s.size(); j++)
+		{
+			if( anagram(s[i]) == anagram(s[j]) )
+			{
+				if (out == false) cout << s[i] << " ";
+				out = true;
+				cout << s[j] << " ";
+			}
+		}
+	}
+
+}
