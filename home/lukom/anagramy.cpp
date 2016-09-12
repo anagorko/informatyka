0c206383d928dfa120ea0d34e4dd675eb05ad45a
Jajanek98 <Janek.lukomski98@gmail.com>
2016-09-13 00:20:54 +0200

    anagramy.cpp optymalizacja wydajności

--- home/lukom/anagramy.cpp
+++ home/lukom/anagramy.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 using namespace std;
-class slownik{
-public:
-	vector <string> w;
-}L;
+
 string sygnatura(string s){
 	int t[256];
 	for(int i=0;i<256;i++) t[i]=0;
@@ -16,6 +14,38 @@ string sygnatura(string s){
 	return ss;
 }
 
+class Anagram
+{
+	string w;
+	string l;
+public:
+	string get() const;
+	string getl() const;
+    Anagram(string s);
+    bool operator<(const Anagram&) const;
+};
+Anagram::Anagram(string s){
+	w = s;
+	l = sygnatura(w);
+}
+string Anagram::get() const{
+	return w;
+}
+string Anagram::getl() const{
+	return l;
+}
+bool Anagram::operator<(const Anagram& a) const{
+	string c = sygnatura(a.get());
+
+	if(l.length() != c.length()) return l.length()<c.length()? 1:0;
+
+	for(int i=0;i<l.length();i++){
+		if(l[i]>c[i]) return 0;
+	}
+	return 1;
+}
+vector <Anagram> L;
+
 int main(){
 	
 	cout<<"podaj ilość słów do słownika:";
@@ -26,17 +56,19 @@ int main(){
 	for(int i=0;i<n;i++){
 		string s;
 		cin>>s;
-		L.w.push_back(s);
+		Anagram a(s);
+		L.push_back(a);
 	}
+	sort(L.begin(), L.end());
+
+//		for(int i=0;i<L.size();i++) cout<<L[i].get()<<endl;
+
 	cout<<"ANAGRAMY:\n";
-	
-	//szukanie anagramu:
-	for(int i=0;i<L.w.size();i++){
-		cout<<L.w[i]<<": ";
-		for(int a=0;a<L.w.size();a++){
-			if(a==i)continue;
-			if(sygnatura(L.w[i])==sygnatura(L.w[a]) )cout<<L.w[a]<<", ";
-		}
-		cout<<"\n";
+
+	cout<<L[0].get()<<": ";
+	for(int i=1;i<L.size();i++){
+		if(L[i-1].getl()==L[i].getl()){ cout<<L[i].get()<<", ";
+		}else{ cout<<endl<<L[i].get()<<": ";}
 	}
+	cout<<endl;
 }
