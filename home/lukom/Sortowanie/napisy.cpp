cd8a73b65accebb9500707ab7c11835bd8ef8d2d
lukom <janek.lukomski98@gmail.com>
2016-09-16 10:46:30 +0200

    napisy.cpp

--- home/lukom/Sortowanie/napisy.cpp
+++ home/lukom/Sortowanie/napisy.cpp
@@ -32,16 +32,19 @@ string rstring::rev(string s) const{
 	return r;
 
 }
+int licz;
 bool rstring::operator<(const rstring& op) const{
 	string ra = rev(w);
 	string rb = rev(op.w);
-
+	return ra  < rb;
+	cout<<licz<<endl;
+	licz++;
 	for(int i=0;i<ra.length() && i<rb.length();i++){
-		if(ra[i]>rb[i]) return 0;
+		if(ra[i]>rb[i]) return false;
 	}
 	int n = ra.length()<rb.length() ? ra.length() : rb.length();
 	if(ra.substr(0,n-1)==rb.substr(0,n-1)) return ra.length()<rb.length() ? 1:0;
-	return 1;
+	return true;
 }
 bool rstring::operator==(const rstring& op) const{
 	if(w.length()!= op.w.length()) return 0;
@@ -59,18 +62,21 @@ istream& operator>>(istream& is, rstring &n){
 	string s;
 	is>>s;
 	n.set(s);
+	return is;
 }
 ostream& operator<<(ostream& os, const rstring &n){
 	os << n.get();
+	return os;
 }
 
 
 int main(){
-	//Wczytywanie
+	licz=0;
 	vector <rstring> L;
-	cout<<"wczytywanie..."<<endl;
+	//Wczytywanie z pliku
+/*	cout<<"wczytywanie..."<<endl;
 	ifstream slownik;
-	slownik.open("polish.txt",ios::in);
+	slownik.open("/home/ki/Pobrane/polish.txt",ios::in);
 	cout<<"opened\n";
 	while(slownik.is_open() && !slownik.eof()){
 		rstring W;
@@ -78,10 +84,17 @@ int main(){
 		L.push_back(W);
 	}
 	if(!slownik.is_open()) cout<<"nie znalazlem pliku"<<endl;
-	slownik.close();
+	slownik.close();*/
+	//wczytywanie z cin
+	while(!cin.eof()){
+		rstring W;
+		cin>>W;
+		L.push_back(W);
+	}
+
 	//wyszukiwanie
 	cout<<"wyszukiwanie...\n";
-//	sort(L.begin(), L.end());
+	sort(L.begin(), L.end());
 	cout<<"ee"<<endl;
 	for(int i=0;i<L.size();i++){
 		cout<<L[i]<<endl;
