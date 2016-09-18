fb6150f3a9280a56ac29bc3f07552e721ddfb036
Miziol <k.miziolek08@gmail.com>
2016-09-18 14:31:29 +0200

    Zadanie punkty.cpp

---
+++ home/Miziol/punkty.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+class point
+{
+public:
+
+	int x,y;
+
+	bool operator == (const point& b)
+	{
+		return (x == b.x && y == b.y);
+	}
+
+	bool operator < (const point& b)
+	{
+		if( x < b.x ) return true;
+		if( x == b.x && y < b.y ) return true;
+
+		return false;
+	}
+
+	bool operator > (const point& b)
+	{
+		if( x > b.x ) return true;
+		if( x == b.x && y > b.y ) return true;
+
+		return false;
+	}
+};
+
+int main()
+{
+	vector <point> p;
+
+	point r;
+
+	while (!cin.eof())
+	{
+		cin >> r.x >> r.y ;
+
+		p.push_back(r); 
+	}
+
+	sort( p.begin(), p.end() );
+
+}
