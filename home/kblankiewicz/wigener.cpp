4eb2110d38e9ba6d2646f94157c54dfc3bbf2839
KrzysiekBlankiewicz <kblankiewicz5@gmail.com>
2016-10-06 18:21:30 +0200

    Działa, tylko wypisuje na cout'a- nie do pliku

--- home/kblankiewicz/wigener.cpp
+++ home/kblankiewicz/wigener.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<fstream>
+
 using namespace std;
 
 int numer(char x)
@@ -12,11 +14,11 @@ string zaszyfruj(string slowo, string klucz)
 	int z;
 	for(int i = 0; i < p.length(); i++)
 	{
-		z = (int) slowo[i] + numer(klucz[i]);
+		z = (int) slowo[i] + numer(klucz[i % klucz.length()]);
 		if(z > 90) {z = z - 26;}
 		p[i] = (char) z;
 	}
-	return p; 
+	return p;
 }
 
 string odszyfruj(string szyfr, string klucz)
@@ -25,18 +27,56 @@ string odszyfruj(string szyfr, string klucz)
 	int z;
 	for(int i = 0; i < s.length(); i++)
 	{
-		z = (int) szyfr[i] - numer(klucz[i]);
+		z = (int) szyfr[i] - numer(klucz[i % klucz.length()]);
 		if(z < 65) {z = z + 26;}
 		s[i] = (char) z;
 	}
 	return s;
 }
 
-int main()
+void zadanie_a(string slowa, string klucze)
 {
-	string x, y, a;
-	cin>>x>>y;
-	cout<<zaszyfruj(x, y)<<endl;
+    ifstream plik1;
+    plik1.open(slowa.c_str(), ios::in);
+
+    ifstream plik2;
+    plik2.open(klucze.c_str(), ios::in);
+
+    cout<<endl<<"zadanie a):"<<endl;
+    string a, b, c;
+    while(!plik1.eof())
+    {
+        plik1>>a;
+        plik2>>b;
+        c = zaszyfruj(a, b);
+        cout<<c<<endl;
+    }
+
 }
 
+void zadanie_b(string slowa, string klucze)
+{
+    ifstream plik1;
+    plik1.open(slowa.c_str(), ios::in);
 
+    ifstream plik2;
+    plik2.open(klucze.c_str(), ios::in);
+
+    cout<<endl<<"zadanie b): "<<endl;
+    string a, b, c;
+    while(!plik1.eof())
+    {
+        plik1>>a;
+        plik2>>b;
+        c = odszyfruj(a, b);
+        cout<<c<<endl;
+    }
+}
+int main()
+{
+    zadanie_a("tj.txt", "klucze1.txt");
+    zadanie_b("sz.txt", "klucze2.txt");
+	/*string x, y, a;
+	cin>>x>>y;
+	cout<<zaszyfruj(x, y)<<endl;*/
+}
