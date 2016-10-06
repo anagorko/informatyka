87c0990208c2cd18930fcafa8fea138a2d4d05f3
Miziol <k.miziolek08@gmail.com>
2016-10-06 14:31:23 +0200

    podpunkt a z zadania szyfr

---
+++ home/Miziol/szyfr_Matura2012.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	string j = "" , h = "", s = "";
+
+	while ( cin >> j )
+	{
+		s = "";
+
+		cin >> h;
+		
+		cout << "..." << j << " " << h << endl;
+		
+		for ( int i = 0; i < j.size(); i++)
+		{
+			int lj = (int) j[i], lh = (int) h[i%h.size()], ls = 0;
+			ls = lj + lh - 64;
+			if( ls > 90) ls = ls - 26;
+			s = s + (char) ls;
+		}
+		cout << s << endl;
+	}
+
+}
