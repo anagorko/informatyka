fe130dedfb53b300235f0936bc32eaf06169215c
Jajanek98 <Janek.lukomski98@gmail.com>
2016-11-18 01:12:38 +0100

    gra_w_zycie.cpp cale zadanie

--- home/lukom/gra_w_zycie.cpp
+++ home/lukom/gra_w_zycie.cpp
@@ -1,36 +1,83 @@
 #include<iostream>
-using namepspace std;
-const int n=12,m=24;
+using namespace std;
+const int n=12,m=20;
 class plansza {
 public:
-	string t[n][m]
+	string t[n];
+	string tp[n];
+	void nstep();
+	int spkt(int x, int y);
+	void draw();
+	int szyw();
 
 
-
+};
+int plansza::szyw(){
+	int s=0;
+	for(int i=0;i<n;i++){
+		for(int a=0;a<m;a++){
+			if(t[i][a] == 'X') s++;
+		}
+	}
+	return s;
+}
+void plansza::draw(){
+	for(int i=0;i<n;i++){
+		cout<<t[i]<<endl;
+	}
+	cout<<endl;
+}
+int plansza::spkt(int x, int y){
+	int s = 0;
+	for(int i=((x+m-1)%m);i<((x+2)%m);i++){
+		if(t[(y+n-1)%n][i]=='X')s++;
+		if(t[(y+1)%n][i]=='X')s++;
+	}
+	if(t[y][((x+m-1)%m)] == 'X') s++;
+	if(t[y][((x+1)%m)] == 'X') s++;
+	return s;
+}
+void plansza::nstep(){
+	string t1[n];
+	for(int i=0;i<n;i++){
+		for(int a=0;a<m;a++){
+			if(t[i][a]=='X'){
+				if(spkt(a,i) == 2 || spkt(a,i) == 3) t1[i] += 'X';
+				else t1[i] += '.';
+			}else{
+				if(spkt(a,i) == 3) t1[i] += 'X';
+				else t1[i] += '.';
+			}
+		}
+	}
+	for(int i=0;i<n;i++){
+		tp[i] = t[i];
+		 t[i] = t1[i];
+	}
 }
 
-
-int naookolo(string P[n*m], int i, int a){
-	for(int q= (i + n - 1)%n;q < (i + n + 1)%n;q++){
-		if
-}
-string zmiana(string P[12*24]){
-	string P1[n*m];
-	for(int i=0;i<12;i++){
-		for(int a=0;a<24;a++){
-			if(naokolo(P, i, a)==3)
-
-}
 int main(){
 	plansza P;
 	//wczytywanie
-	string s;
-	int wys=0,szer=0;
+	for(int i=0;i<n;i++) cin>>P.t[i];
 
-
-	string P[12*20];//20 kolumn
-	for(int i=0;i<37;i++){
-	string P = zmiana(P)
-
+	P.draw();
+	bool juz_powiedzialem = false;
+	for(int i=0;i<100;i++){
+		P.nstep();
+//		P.draw();
+		if(i==36) cout<<"Sasiedzi w 37 pokoleniu punktu (2,19): "<< P.spkt(1,18)<<endl;
+		if(i==1) cout<<"żywych komórek w drugim pokoleniu: "<<P.szyw()<<endl;;
+		bool takie_samo = true;
+		for(int q=0;q<n;q++){
+			if(P.tp[q] != P.t[q]) takie_samo=false;
+		}
+		if(!juz_powiedzialem && takie_samo){
+			cout<<"układ się ustalił w pokoleniu ";
+			cout<<i+1<<endl;
+			cout<<"liczba zywych komórek to: "<<P.szyw()<<endl;
+			juz_powiedzialem = true;
+		}
 	}
+
 }
