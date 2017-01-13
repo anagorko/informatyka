5a6b2b0ea97490c6e6f548bddfe0743777b8f1a1
Kacper <k.miziolek08@gmail.com>
2017-01-13 08:14:53 +0100

    Tu jest błąd "

--- home/Miziol/Database/dziennik_ocen.sql
+++ home/Miziol/Database/dziennik_ocen.sql
@@ -1,35 +1,40 @@
 USE dziennik_ocen
 
-DROP TABLE IF EXIST uczniowie
-CREATE TABLE uczniowie(
-	VARCHAR(8) id_ucznia,
-	VARCHAR(100) imie,
-	VARCHAR(100) nazwisko,
-	VARCHAR(4) klasa
+DROP TABLE IF EXISTS uczniowie;
+CREATE TABLE uczniowie
+	(
+	id_ucznia CHAR(8),
+	imie CHAR(100),
+	nazwisko CHAR(100),
+	klasa CHAR(4)
+	);
 
-);
+LOAD DATA LOCAL INFILE 'uczniowie.txt' INTO TABLE uczniowie IGNORE 1 LINES;
 
-LOAD DATA LOCAL INFILE uczniowie.txt INTO TABLE uczniowie IGNORE 1 LINES;
+DROP TABLE IF EXISTS przedmioty;
+CREATE TABLE przedmioty
+	(
+	id_przedmiotu INT,
+	nazwa_przedmiotu CHAR(100)
+	);
 
-DROP TABLE IF EXIST przedmioty
-CREATE TABLE przedmioty(
-	INT id_przedmiotu,
-	VARCHAR(100) nazwa_przedmiotu
-);
+LOAD DATA LOCAL INFILE 'przedmioty.txt' INTO TABLE przedmioty IGNORE 1 LINES;
 
-LOAD DATA LOCAL INFILE przedmioty.txt INTO TABLE przedmioty IGNORE 1 LINES;
+DROP TABLE IF EXISTS oceny;
+CREATE TABLE oceny
+	(
+	id_oceny INT,
+	data DATE,
+	id_ucznia CHAR(8),
+	id_przedmiotu INT,
+	ocena INT
+	);
 
-DROP TABLE IF EXIST oceny
-CREATE TABLE oceny(
-	INT id_oceny,
-	DATA data,
-	VARCHAR(8) id_ucznia,
-	INT id_przedmiotu,
-	INT ocena
-);
+LOAD DATA LOCAL INFILE 'oceny.txt' INTO TABLE oceny IGNORE 1 LINES;
 
-LOAD DATA LOCAL INFILE oceny.txt INTO TABLE oceny IGNORE 1 LINES;
-
-SELECT klasa
-FROM uczniowie
-WHERE 
+SELECT ile_d.klasa
+FROM 
+	(SELECT klasa, COUNT( imie LIKE '%a') as d FROM uczniowie GROUP BY klasa) as ile_d,
+	(SELECT klasa, COUNT( imie NOT LIKE '%a') as m FROM uczniowie GROUP BY klasa) ile_m
+WHERE ile_d.klasa = ile_m.klasa AND ile_d.d < ile_m.m
+GROUP BY ile_d.klasa \G
