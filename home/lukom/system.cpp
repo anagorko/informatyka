eb5dc340ce3b1996fd2601101c21e3b4c0be980b
lukom <janek.lukomski98@gmail.com>
2017-01-13 08:29:49 +0100

    system.cpp dokonczone

--- home/lukom/system.cpp
+++ home/lukom/system.cpp
@@ -1,6 +1,14 @@
 #include<iostream>
 using namespace std;
 string cyfra = "0123456789ABCDEF";
+int z_ciagu_cyfr(char a){
+	for(int i=0;i<cyfra.length();i++){
+		if(a == cyfra[i]){
+			return i;
+		}
+	}
+	return -100000;
+}
 string na_system(int n, int b){
 	string s = "";
 	bool minus = 0;
@@ -19,12 +27,12 @@ int z_systemu(string l, int b){
 	int n=0;
 	bool minus = 0;
 	int i=0;
-	if(l[0]="-"){
+	if(l[0]=='-'){
 		minus = true;
 		i=1;
 	}
 	for(;i<l.length();i++){
-		n = n*b + (int)l[i];//do poprawy
+		n = n*b + z_ciagu_cyfr(l[i]);
 	}
 	return minus? -n : n;
 }
@@ -33,6 +41,6 @@ int main(){
 	int b;
 	cin>>n;
 	cin>>b;
-	cout<<na_system(n,b)<<endl;
+	cout<<z_systemu(na_system(n,b), b)<<endl;
 return 0;
 }
