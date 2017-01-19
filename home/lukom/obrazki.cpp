335209e1d2931305e91280a742e5aea78970df88
Jajanek98 <Janek.lukomski98@gmail.com>
2017-01-19 16:41:09 +0100

    obrazki.cpp dokonczenie

--- home/lukom/obrazki.cpp
+++ home/lukom/obrazki.cpp
@@ -9,34 +9,50 @@ class obrazek {
 	bool rekurencyjny() const;
 	void wypisz();
 	bool poprawny() const;
-	void blad(int &rzad, int &kolu) const;
+	bool naprawialny(int &a, int &b);
+	bool nienaprawialny() const;
+	int liczba_bledow() const;
+	int sum_rz(int n) const;
+	int sum_ko(int n) const;
 
 }O[200];
-void obrazek::blad(int &rzad, int&kolu) const {
-	rzad =0; kolu =0;
+int obrazek::liczba_bledow() const {
+	int w=0;
 	for(int i=0;i<20;i++){
-                int kol=0;
-                int rze=0;
-                for(int j=0;j<20;j++){
-                        if(s[i][j] == '1') rze++;
-                        if(s[j][i] == '1') kol++;
-                }
-		if((int)s[i][20] - '0' != (rze%2)) rzad++;
-                if((int)s[20][i] - '0' != (kol%2)) kolu++;
+		if((sum_rz(i)%2) != (int)s[i][20] - '0') w++;
+                if((sum_ko(i)%2) != (int)s[20][i] - '0') w++;
 	}
+	return w;
+}
+int obrazek::sum_rz(int n) const {
+	int w=0;
+	for(int i = 0; i< 20;i++) if(s[n][i] == '1') w++;
+	return w;
+}
+int obrazek::sum_ko(int n) const {
+	int w = 0;
+	for(int i=0;i<20;i++) if(s[i][n] == '1') w++;
+	return w;
+
+}
+bool obrazek::naprawialny(int &a, int &b){
+	if(poprawny()) return false;
+	int bledy_pio = 0;
+	int bledy_poz = 0;
+	for(int i=0;i<20;i++){
+		if((sum_rz(i)%2) != (int)s[i][20] - '0'){ bledy_poz++; a  = i+1; }
+		if((sum_ko(i)%2) != (int)s[20][i] - '0'){ bledy_pio++; b = i+1 ;}
+	}
+	if(bledy_pio == 1 && bledy_poz == 1) return true;
+	else return false;
 }
 bool obrazek::poprawny() const {
 	for(int i=0;i<20;i++){
-		int kol=0;
-		int rze=0;
-		for(int j=0;j<20;j++)
-			if(s[i][j] == '1') rze++;
-			if(s[j][i] == '1') kol++;
-		}
-		if((int)s[i][20] - '0' != (rze%2)) return false;
-		if((int)s[20][i] - '0' != (kol%2)) return false;
+		if((sum_rz(i)%2) != (int)s[i][20] - '0') return false;
+		if((sum_ko(i)%2) != (int)s[20][i] - '0') return false;
 	}
 	return true;
+
 }
 bool obrazek::rekurencyjny() const {
 	for(int i=0;i<10;i++){
@@ -97,11 +113,16 @@ void pyt2(){
 	cout<<"obrazow rekurencyjnych jest: "<<ile<<endl;
 }
 void pyt3(){
-
+	for(int i=0;i<200;i++){
+		int a=-1, b=-1;
+		if(O[i].naprawialny(a,b)){
+			cout<<"naprawialny"<<i+1<<" : "<<a<<" "<<b<<endl;
+		}
+	}
 }
 int main(){
 	wczytanie();
 //	pyt1();
 //	pyt2();
-	pyt3();
+//	pyt3();
 }
