7c6287413cc903c01dba5c5f9a87bd23016d4c9e
Kacper <k.miziolek08@gmail.com>
2017-01-24 19:00:34 +0100

    Rozwiazanie ciagi zerojedynkowe

--- home/Miziol/ciagi_zerojedynkowe.cpp
+++ home/Miziol/ciagi_zerojedynkowe.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
+#include<math.h>
 
 using namespace std;
+
+bool pp[262145];
+bool p[513];
 
 bool cykliczna(string t)
 {
@@ -26,13 +30,55 @@ bool jedynki(string t)
 	return true;
 }
 
+void pol_pierwsze()
+{
+	for(int i = 0; i < 262145; i++)
+	{
+		pp[i] = false;
+		if( i < 513 ) p[i] = true;
+	}
+//pierwsze
+
+	for ( int i = 2; i < 513; i++ )
+	{
+		if ( p[i] )
+			for ( int j = i * 2; j < 513; j = j + i)
+				p[j] = false;
+	}
+
+//polpierwsze
+	for ( int i = 2; i < 513; i++)
+	{
+		for ( int j = i; j < 513; j++ )
+		{
+			if( p[i] && p[j] )
+				pp[ i * j] = true;
+		}
+	}
+}
+
+int bin_dec(string s)
+{
+	int w = 0;
+
+	for ( int p = 0; p < s.size(); p++ )
+	{
+		w = w * 2 + ( (int) s[p] - 48);
+	}
+
+	return w;
+}
 
 
 int main()
 {
 	string s;
 
-	int cykliczne = 0, jedynkowe = 0;
+	int min, max, r;
+
+	int cykliczne = 0, jedynkowe = 0, polp = 0;
+
+	pol_pierwsze();
 
 	for ( int i = 0; i < 1000; i++ )
 	{
@@ -41,7 +87,22 @@ int main()
 		if ( cykliczna (s) ) cykliczne++;
 		if ( jedynki (s) ) jedynkowe++;
 
+		r = bin_dec(s);
+
+		if ( i == 0 )
+		{
+			min = r;
+			max = r;
+		}
+
+		if ( pp[r] )
+		{
+			polp++;
+			if ( min > r ) min = r;
+			if ( max < r ) max = r;
+		}
+
 	}
 
-	cout << cykliczne << endl << jedynkowe << endl;
+	cout << cykliczne << endl << jedynkowe << endl << polp << " " << max << " " << min;
 }
