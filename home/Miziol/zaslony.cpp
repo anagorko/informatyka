27d0e4ec62c41ae921a970ed0233a9d66be4b5ff
Miziol <k.miziolek08@gmail.com>
2017-02-22 15:58:44 +0100

    Dokonczenie zaslony

--- home/Miziol/zaslony.cpp
+++ home/Miziol/zaslony.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
 
 double f( int x )
@@ -27,6 +28,40 @@ void podpunkt_a(double x_l, double x_p)
 	cout << "a) " << pole << endl;
 }
 
+void podpunkt_b( double x_p, double x_l, double y_g, double y_d)
+{
+	double obw = 0.00;
+
+	int n = 1000; //dokladnosc
+
+	double x_przes = ( x_p - x_l ) / n;
+
+	obw = obw + 2 * ( x_p - x_l ) + y_g - y_d;
+
+	for ( double x = x_l; x < x_p; x = x + x_przes )
+	{
+		obw = obw + sqrt( pow( x_przes, 2 ) + ( pow( g( x + x_przes) - g( x ), 2 ) ) );
+
+		obw = obw + sqrt( pow( x_przes, 2 ) + ( pow( f( x + x_przes) - f( x ), 2 ) ) );
+	}
+
+	cout << "b) " << obw << endl;
+}
+
+void podpunkt_c( double x_l, double x_p )
+{
+	double x_przes = 0.25;
+
+	int dlugosc = 0;
+
+	for ( double x = x_p; x > x_l; x = x - x_przes )
+	{
+		dlugosc = dlugosc + ( (double) f( x - x_przes ) - (double) g( x - x_przes ) );
+	}
+
+	cout << "c) " << dlugosc << endl;
+}
+
 int main()
 {
 	double y_g, y_d, x_l, x_p;
@@ -36,4 +71,8 @@ int main()
 	x_p = 10.00;
 
 	podpunkt_a( x_l, x_p );
+
+	podpunkt_b( x_p, x_l, y_g, y_d );
+
+	podpunkt_c( x_l, x_p );
 }
