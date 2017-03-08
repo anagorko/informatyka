5a2ea699b76fd66d9068e372b99f9c673ee39b25
Jajanek98 <Janek.lukomski98@gmail.com>
2017-03-08 15:00:29 +0100

    dziala,chociaz nie wychodzą za ładne obrazy

--- home/lukom/Grafika/konwerter_odcieni_szarosci.cpp
+++ home/lukom/Grafika/konwerter_odcieni_szarosci.cpp
@@ -8,7 +8,7 @@ char *t;
 
 
 void wczytanie(){
-	ifstream plik("/home/janek/Pobrane/tapeta.bmp", ios::binary);
+	ifstream plik("/home/janek/Pobrane/tulipany (1).bmp", ios::binary);
 	char c;
 	char head[2];
 	char sizec[4];
@@ -30,7 +30,7 @@ void wczytanie(){
 	plik.close();
 }
 void zapisanie(){
-	ofstream plik ("tapeta.bmp", ios::binary);
+	ofstream plik ("tulipany.bmp", ios::binary);
 	for(int i=0;i<size;i++)	plik.put(t[i]);
 	plik.close();
 }
@@ -41,20 +41,24 @@ void filtr(){
 	int height = *((int*)(t + 22));
 	int num_color_planes = *((int*)(t + 26));
 	int bits_per_pixel = *((int*)(t + 28));
-	int size_of_raw_bitmap = *((int*)(t + 3443));
-	cout<<size_of_raw_bitmap<<endl;
+	int size_of_raw_bitmap = *((int*)(t + 0x22));
+	cout<<bits_per_pixel<<"-bitowy"<<endl;
+	cout<<"size = "<<size<<endl;
+	cout<<800*600*3<<endl;
+	cout<<(int)t[offset]<<endl;
 	int zw = 0;
-	for(int i=0;i<width;i++){
-		for(int j=0;j<height;j++){
-			int srednia = (t[offset + zw  + 3 * i] + t[offset + zw + 3 * i + 1] + t[offset + zw + 3 * i + 2])/3;
-			t[offset + zw + 3 * i] = srednia;
-			t[offset + zw + 3 * i + 1] = srednia;
-			t[offset + zw + 3 * i + 2] = srednia;
+	for(int i=1;i<=height;i++){
+		for(int j=1;j<=width;j++){
+			int pix = offset + 3*j + i*3*width;
+			int sre = (t[pix] + t[pix + 1] + t[pix+2])/3;
+			t[pix] = sre;
+			t[pix + 1] = sre;
+			t[pix + 2] = sre;
 		}
-		zw++;
 	}
 }
 int main(){
+	cout<<"poram dziala dla pikow .bmp 24 bitowych\n";
 	wczytanie();
 	filtr();
 	zapisanie();
