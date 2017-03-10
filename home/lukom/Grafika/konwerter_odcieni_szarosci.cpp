45a8719355e7cd08ff7fa710734cf411d181743a
Jajanek98 <Janek.lukomski98@gmail.com>
2017-03-10 15:52:13 +0100

    BMP editor dodanie funkcji char_to_int()

--- home/lukom/Grafika/konwerter_odcieni_szarosci.cpp
+++ home/lukom/Grafika/konwerter_odcieni_szarosci.cpp
@@ -3,9 +3,14 @@
 #include<vector>
 using namespace std;
 
-int size;
 char *t;
 
+int char_to_int(char *w, int d){//wskaźnik do liczby, ilosc bitow kodujących liczbę
+	char c[4] = {0, 0, 0, 0};
+	for(int i=0;i<d && i < 4;i++) c[i] = w[i];
+	int wyn = *((int*)c);
+	return wyn;
+}
 
 void wczytanie(){
 	ifstream plik("/home/janek/Pobrane/tulipany (1).bmp", ios::binary);
@@ -15,7 +20,7 @@ void wczytanie(){
 	for(int i=0;i<2;i++) plik.get(head[i]);
 	for(int i=0;i<4;i++) plik.get(sizec[i]);
 
-	size = *((int*)sizec);
+	int size = char_to_int(sizec, 4);
 
 	t = new char[size];
 
@@ -30,27 +35,30 @@ void wczytanie(){
 	plik.close();
 }
 void zapisanie(){
+	int size = *((int*)(t + 2));
 	ofstream plik ("tulipany.bmp", ios::binary);
 	for(int i=0;i<size;i++)	plik.put(t[i]);
 	plik.close();
+	cout<<"\n";
 }
 void filtr(){
-	int offset = *((int*)(t + 2 * 3 + 4 ));
-	int size_of_dib_header = *((int*)(t + 14));
-	int width = *((int*)(t + 18));
-	int height = *((int*)(t + 22));
-	int num_color_planes = *((int*)(t + 26));
-	int bits_per_pixel = *((int*)(t + 28));
-	int size_of_raw_bitmap = *((int*)(t + 0x22));
+	int size = char_to_int(t + 2, 4);
+	int offset = char_to_int( t + 0xA, 4 );
+	int size_of_dib_header = char_to_int( t + 0xE, 4);
+	int width = char_to_int( t + 0x12, 4);
+	int height = char_to_int( t + 0x16, 4);
+	int num_color_planes = char_to_int( t + 0x1A, 2);
+	int bits_per_pixel = char_to_int( t + 0x1C, 2);
+	int size_of_raw_bitmap = char_to_int( t + 0x22, 4);
+
 	cout<<bits_per_pixel<<"-bitowy"<<endl;
 	cout<<"size = "<<size<<endl;
-	cout<<800*600*3<<endl;
-	cout<<(int)t[offset]<<endl;
+
 	int zw = 0;
 	for(int i=1;i<=height;i++){
 		for(int j=1;j<=width;j++){
 			int pix = offset + 3*j + i*3*width;
-			int sre = (t[pix] + t[pix + 1] + t[pix+2])/3;
+			char sre = (t[pix] + t[pix + 1] + t[pix+2])/3;
 			t[pix] = sre;
 			t[pix + 1] = sre;
 			t[pix + 2] = sre;
@@ -58,7 +66,7 @@ void filtr(){
 	}
 }
 int main(){
-	cout<<"poram dziala dla pikow .bmp 24 bitowych\n";
+	cout<<"program dziala dla pikow .bmp 24 bitowych\n\n";
 	wczytanie();
 	filtr();
 	zapisanie();
