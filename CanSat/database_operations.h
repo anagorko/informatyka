ac0728a246a423122a25a996ec795d902fd5d3df
Miziol <k.miziolek08@gmail.com>
2017-03-24 19:56:20 +0100

    Wszystko

--- CanSat/database_operations.h
+++ CanSat/database_operations.h
@@ -5,10 +5,104 @@
 using namespace std;
 
 
-struct Spectrum
+
+struct spectrum
 {
 	int temperature;
 	int preasure;
 
 	int lfl[256];
 };
+
+spectrum SELECT( char *query )
+{
+	spectrum uncode;
+
+	sqlite3 *database;
+
+	sqlite3_open( "CanSat.db", &database );
+
+//tu się zaczyna ctrl-c
+
+	sqlite3_stmt *statement;
+
+	if ( sqlite3_prepare( database, query, -1, &statement, 0 ) == SQLITE_OK ) 
+	{
+		int ctotal = sqlite3_column_count(statement);
+		int res = 0;
+
+		while ( 1 )         
+		{
+			res = sqlite3_step(statement);
+
+			if ( res == SQLITE_ROW ) 
+			{
+				for ( int i = 0; i < ctotal; i++ ) 
+				{
+					string s = (char*)sqlite3_column_text(statement, i);
+                    // print or format the output as you want 
+					cout << s << " " ;
+				}
+				cout << endl;
+			}
+
+			if ( res == SQLITE_DONE || res==SQLITE_ERROR)    
+			{
+				cout << "done " << endl;
+				break;
+			}
+		}
+	}
+
+//tu się kończy ctrl-v
+
+	sqlite3_close( database );
+
+	return uncode;
+}
+
+spectrum lastRecord()
+{
+	return SELECT( "SELECT * FROM data ORDER BY time DESC LIMIT 1" );
+}
+
+
+
+
+
+
+//Kod z internetu
+/*void getTableData()
+{
+    sqlite3_stmt *statement;    
+
+    char *query = "select * from data";
+
+    if ( sqlite3_prepare( database, query, -1, &statement, 0 ) == SQLITE_OK ) 
+    {
+        int ctotal = sqlite3_column_count(statement);
+        int res = 0;
+
+        while ( 1 )         
+        {
+            res = sqlite3_step(statement);
+
+            if ( res == SQLITE_ROW ) 
+            {
+                for ( int i = 0; i < ctotal; i++ ) 
+                {
+                    string s = (char*)sqlite3_column_text(statement, i);
+                    // print or format the output as you want 
+                    cout << s << " " ;
+                }
+                cout << endl;
+            }
+            
+            if ( res == SQLITE_DONE || res==SQLITE_ERROR)    
+            {
+                cout << "done " << endl;
+                break;
+            }    
+        }
+    }
+}*/
---
+++ CanSat/test.cpp
@@ -0,0 +1,12 @@
+#include"database_operations.h"
+
+using namespace std;
+
+int main()
+{
+	spectrum s;
+
+	s = lastRecord();
+
+	cout << s.temperature << endl;
+}
