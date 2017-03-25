d46f83ffe48f1c96ab89dbd160e38c1a7af323a1
Andrzej Nagórko <amn@mimuw.edu.pl>
2017-03-25 09:31:18 +0100

    Wydzielona klasa CanSatViewerWindow

--- CanSat/viewer/config.h
+++ CanSat/viewer/config.h
@@ -10,5 +10,9 @@ const int lfl_size = 256;
 const int viewer_width = 1200;
 const int viewer_height = 768;
 
+// Update frame rate
+
+const float FPS = 60;       // Frames Per Second
+
 #endif /* __CONFIG__ */
 
