8dd71e324f37f3883a9d86a4342f6c92b47158b4
Miziol <k.miziolek08@gmail.com>
2017-03-28 13:31:24 +0200

    Poprawki w definicjach database do sprwadzenia

---
+++ CanSat/datastore/datastore.h
@@ -0,0 +1,24 @@
+#ifndef __DATABASE__
+#define __DATABASE__
+
+#include<sqlite3.h>
+#include "../spectrogram.h"
+
+using namespace std;
+
+
+int chars_to_int( char c1, char c2 );
+
+std::ostream operator<< ( ostream& o, Spectrogram& in );
+
+Spectrogram SELECT( char *query );
+
+Spectrogram lastRecord();
+
+
+//Kod z internetu
+/*void getTableData();
+*/
+
+
+#endif /* __DATABASE__ */
