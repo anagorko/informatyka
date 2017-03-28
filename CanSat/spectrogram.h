eb8deacb88d1266b054bc296d0758b574e4ec53e
Miziol <k.miziolek08@gmail.com>
2017-03-28 13:50:19 +0200

    Poprawki

--- CanSat/spectrogram.h
+++ CanSat/spectrogram.h
@@ -5,10 +5,13 @@ class Spectrogram {
 public:
 	static const int resolution = 256;
 
-    int lfl[resolution];
+	int lfl[resolution];
 
-    int moment;
+	int moment;
+	
 	int temperature, pressure;
+
+	string time;
 };
 
 #endif /* __SPECTROGRAM__ */
