7594da5aeee71a76997868eb11afa9d740032dcf
Jajanek98 <Janek.lukomski98@gmail.com>
2017-03-31 09:46:12 +0200

    dodanie wyświatlania wykresu diffrent

--- CanSat/viewer/graph.cpp
+++ CanSat/viewer/graph.cpp
@@ -1,6 +1,6 @@
 #include <allegro5/allegro_primitives.h>
 #include "graph.h"
-
+#include "../spectrogram.h"
 void Graph::draw(Spectrogram s, ALLEGRO_DISPLAY * display) const {
 
     al_draw_rectangle(position_x, position_y, position_x + width, position_y + height, al_map_rgb(255,255,255), frame_thickness);
@@ -9,15 +9,30 @@ void Graph::draw(Spectrogram s, ALLEGRO_DISPLAY * display) const {
     
     
     for(int i=0;i< Spectrogram::resolution;i++){
-        switch(typeShow){
-            case 0:
-                float y1 = (position_y + height - frame_thickness) - (s.lfl[i] * (height - 2 * frame_thickness))/1023;
-                float x2 = x1 + column_thickness;
-                float y2 = position_y + height - frame_thickness;
-                al_draw_filled_rectangle(x1, y1, x2, y2,al_map_rgb(23,255,100));
-                x1 = x2;
-                break;
-        
+
+        float y1;
+        float x2;
+        float y2;
+        float value;
+        for(int j=0;j<V.size();j++){
+            switch(typeShow){
+                case 0:
+                    value = V[j].lfl[i];
+                    y1 = (position_y + height - frame_thickness) - (value * (height - 2 * frame_thickness))/1023;
+                    x2 = x1 + column_thickness;
+                    y2 = position_y + height - frame_thickness;
+                    al_draw_filled_rectangle(x1, y1, x2, y2,al_map_rgb(23,255,100));
+                    x1 = x2;
+                    break;  
+                 case 1:
+                    value = V[j].lfl[i] - basis.lfl[i];
+                     y1 = (position_y + height / 2 - frame_thickness) - (value * (height / 2 - 2 * frame_thickness))/1023;
+                     x2 = x1 + column_thickness;
+                     y2 = position_y + height / 2 - frame_thickness;
+                     al_draw_filled_rectangle(x1, y1, x2, y2,al_map_rgb(23,255,100));
+                     x1 = x2;
+                     break;
+            }
         }
     }
 
