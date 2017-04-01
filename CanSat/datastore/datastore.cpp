9c25ea30683535062ba1818faec2170cbc01c90f
Miziol <k.miziolek08@gmail.com>
2017-04-01 21:27:57 +0200

    Nie do końca dziłająca funkcja moment to string

--- CanSat/datastore/datastore.cpp
+++ CanSat/datastore/datastore.cpp
@@ -267,17 +267,19 @@ int time_to_moment( string time )
 string moment_to_time( int moment )
 {
 	struct tm zero;
-	zero.tm_year = 2017;
+	zero.tm_year = 117;
 	zero.tm_mon = 0;
 	zero.tm_mday = 1;
 	zero.tm_hour = 0;
 	zero.tm_min = 0;
 	zero.tm_sec = 0;
-	
-	time_t now = mktime( &zero ) + moment;
 
-	char buff[20];
-	strftime(buff, 20, "%Y-%m-%d %H:%M:%S", localtime( &now ) );
+	time_t now = mktime( &zero ) + ( moment / 10 );
 
-	stringstream strm;
+	char buff[19];
+	strftime(buff, 19, "%Y-%m-%d %H:%M:%S", localtime( &now ) );
+
+	string s = buff;
+
+	return s + '.' + (char) ( (moment % 10) + '0' );
 }
