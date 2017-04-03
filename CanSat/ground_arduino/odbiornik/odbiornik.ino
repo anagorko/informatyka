1d9a60691fadd31903c96cd929e73936cd9211f7
Miziol <k.miziolek08@gmail.com>
2017-04-03 16:54:00 +0200

    Dekompresja dodadna do odbiornika

--- CanSat/ground_arduino/odbiornik/odbiornik.ino
+++ CanSat/ground_arduino/odbiornik/odbiornik.ino
@@ -14,10 +14,63 @@
  */
 #include <qbcan.h>
 
+#define temp_zero -300 //punkt zero dla temperatury (wartość w dC)
+#define pres_zero 600 //punkt zero dla ciśnienia (wartość w hPa)
+#define ilosc_bajtow 166
+
 constexpr int NodeID = 1;
 constexpr int NetworkID = 233;
 
 RFM69 radio;
+
+char data[ilosc_bajtow];
+
+int id_serii, id_measure, temperature, pressure, lfl[256];
+
+void dekompresja( int *seria, int *pomiar, int *temp, int *pres, int spekt[] )
+{
+  *seria = (int) data[0];
+  *pomiar = ( (int) data[1] ) * 256 + (int) data[2];
+
+  *temp = (int) data[3] * 4 + ( (int) data[4] / 64 );
+  *pres = ( (int) data[4] % 64 * 16 ) + ( (int) data[5] / 16 );
+
+  *temp = *temp + temp_zero;
+  *pres = *pres + pres_zero;
+
+  int spektrometr = 0, robocza = ( (int) data[5] % 16), reszta = 4;
+
+//cout << (int) data[5] << " test " << robocza << endl;
+
+  for ( int i = 6; i < ilosc_bajtow; i++ )
+  {
+    if ( spektrometr >= 256 )
+      break;
+
+    robocza = robocza * 256 + (int) data[i];  
+
+//cout << (int) data[i] << " test " << robocza << endl;
+
+    reszta = reszta + 8 - 10;
+
+    if ( reszta < 0 )
+    {
+      i++; 
+
+      robocza = robocza * 256 + (int) data[i];
+
+      reszta = reszta + 8;
+    }
+
+    spekt[spektrometr] = robocza / (int) pow( 2, reszta );
+    spekt[spektrometr + 1] = robocza / (int) pow( 2, reszta );
+
+    robocza = robocza % (int) pow( 2, reszta );
+
+    spektrometr += 2;
+  }
+}
+
 
 void setup() {
   Serial.begin(9600);
@@ -35,9 +88,21 @@ void loop() {
   if(radio.receiveDone()){
     Serial.print("Incoming packet: ");
     for(byte i = 0; i < radio.DATALEN; i++){
-      Serial.print((char)radio.DATA[i]);
+      data[i] = (char)radio.DATA[i];
     }
     Serial.print(" end message. ");
     Serial.println("RSSI: " + String(radio.RSSI));
+  }
+
+  dekompresja( &id_serii, &id_measure, &temperature, &pressure, &lfl[0]);
+
+  Serial.print( id_serii ); Serial.print( ";" );
+  Serial.print( id_measure ); Serial.print( ";" );
+  Serial.print( temperature ); Serial.print( ";" );
+  Serial.print( pressure ); Serial.print( ";" );
+
+  for ( int i = 0; i < 256; i++ )
+  {
+    Serial.print( lfl[i] ); Serial.print( ";" );
   }
 }
