4049557a099302f698db495e188fbe908f043ab0
Miziol <k.miziolek08@gmail.com>
2017-04-03 13:46:33 +0200

    Kompresja z poprawka

---
+++ CanSat/arduino/kompresja_danych_1.0.0.cpp
@@ -0,0 +1,156 @@
+#include<iostream>
+#include<math.h>
+
+
+using namespace std;
+
+#define temp_zero -300 //punkt zero dla temperatury (wartość w dC)
+#define pres_zero 600 //punkt zero dla ciśnienia (wartość w hPa)
+#define ilosc_bajtow  255 //ilość bajtów do zapisu
+
+//kod zapisuje w 44 bity kod serii, kod pomiaru, temperature i ciśnienie
+//oraz tyle pomairów ile jest możliwe ze spektrometru
+
+unsigned char data[ilosc_bajtow];
+
+
+void kompresja( int *seria, int *pomiar, int *temp, int *pres, int spekt[] )
+{
+	data[0] = (char) *seria;
+	data[1] = (char) ( *pomiar / 256 );
+	data[2] = (char) ( *pomiar % 256 );
+
+	int t = *temp - temp_zero;
+	int p = *pres - pres_zero;
+
+	data[3] = (char) ( t / 4 );
+
+	p = p + ( (t % 4) * 1024 );
+
+	data[4] = (char) ( p / 16 );
+
+	int spektrometr = 0, robocza = (p % 16) * 1024 + spekt[spektrometr], reszta = 6;
+
+	data[5] = (char) ( robocza / 64 );
+
+	robocza = robocza % 64;
+
+	spektrometr++;
+
+	for ( int i = 6; i < ilosc_bajtow; i++ )
+	{
+		if ( spektrometr >= 256 )
+			break;
+
+		robocza = robocza * 1024 + spekt[spektrometr];
+
+		reszta = reszta + 10 - 8;
+
+		data[i] = (char) ( robocza / pow( 2, reszta ) );
+
+//cout << "r" << robocza << "." << pow( 2, reszta ) << "=" << (int) data[i] << " ";
+
+		robocza = robocza % (int) pow( 2, reszta );
+
+		if ( reszta == 8 )
+		{
+			i++; 
+
+			data[i] = (char) robocza;
+
+			robocza = 0;
+
+			reszta = 0;
+		}
+
+		spektrometr += 2;
+	}
+
+	//tutaj trzeba posłać to od radia
+
+/*cout << spekt[1] << endl <<"\n\n\n po konwersji\n";
+
+for ( int i = 0; i < 255; i++ )
+{
+	cout << (int) data[i] << " ";
+}*/
+
+}
+
+
+void dekompresja( int *seria, int *pomiar, int *temp, int *pres, int spekt[] )
+{
+	*seria = (int) data[0];
+	*pomiar = ( (int) data[1] ) * 256 + (int) data[2];
+
+	*temp = (int) data[3] * 4 + ( (int) data[4] / 64 );
+	*pres = ( (int) data[4] % 64 * 16 ) + ( (int) data[5] / 16 );
+
+	*temp = *temp + temp_zero;
+	*pres = *pres + pres_zero;
+
+	int spektrometr = 0, robocza = ( (int) data[5] % 16), reszta = 4;
+
+//cout << (int) data[5] << " test " << robocza << endl;
+
+	for ( int i = 6; i < ilosc_bajtow; i++ )
+	{
+		if ( spektrometr >= 256 )
+			break;
+
+		robocza = robocza * 256 + (int) data[i];	
+
+//cout << (int) data[i] << " test " << robocza << endl;
+
+		reszta = reszta + 8 - 10;
+
+		if ( reszta < 0 )
+		{
+			i++; 
+
+			robocza = robocza * 256 + (int) data[i];
+
+			reszta = reszta + 8;
+		}
+
+		spekt[spektrometr] = robocza / (int) pow( 2, reszta );
+
+		robocza = robocza % (int) pow( 2, reszta );
+
+		spektrometr += 2;
+	}
+}
+
+
+
+int main()
+{
+	int s = 45, p = 9509, t = 396, c = 1023;
+	int tablica[255];
+
+	for ( int i = 0; i < 255; i++ )
+	{
+		tablica[i] = 0;
+	}
+
+tablica[0] = 78;
+tablica[1] = 111;
+tablica[2] = 69;
+
+cout << "przed kompresją" << endl;
+cout << s << " " << p << endl << t << endl << c << endl;
+cout << tablica[0] << " " << tablica[1] << " " << tablica[2] << endl; 
+
+	kompresja( &s, &p, &t, &c, &tablica[0] );
+
+tablica[0] = 0;
+tablica[1] = 0;
+tablica[2] = 0;
+
+cout << endl << "po" << endl;
+
+	dekompresja( &s, &p, &t, &c, &tablica[0] );
+
+cout << s << " " << p << endl << t << endl << c << endl;
+cout << tablica[0] << " " << tablica[1] << " " << tablica[2] << endl;
+}
